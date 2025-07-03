c0e2de1ee7e9b2e78037d4c5d16b247cf0406eb0
asenetcky <me@asenetcky.dev>
2025-07-02 21:55:20 -0400

    feat: implement checkmate input validation

--- R/checker_df.R
+++ R/checker_df.R
@@ -23,27 +23,47 @@
 #' @export
 #'
 #' @examples
-#' x <- data.frame(x = c('2', '*', '*', '3', '5', '2'),y = c('*', '3', '5', '*', '2', '*'))
+#' x <-
+#'  data.frame(
+#'    x = c('2', '*', '*', '3', '5', '2'),
+#'    y = c('*', '3', '5', '*', '2', '*')
+#' )
 #'
 #' checker_df(x, c('x','y'), '\\*')
 #' checker_df(x, c('x'), '2')
 checker_df <- function(df, supp_col, regex_char) {
-  stopifnot(
-    inherits(df, 'data.frame'),
-    inherits(regex_char, 'character') | inherits(regex_char, 'numeric'),
-    nrow(df) > 0,
-    ncol(df) > 0 ,
-    regex_char != '',
-    length(supp_col) > 0,
-    supp_col %in% names(df)
-    )
-  if (inherits(regex_char,'numeric')) {
-    regex_char = as.character(regex_char)
-  }
-  checker <- df |>
-    # If a value has been suppressed, replace it with 1.
-    # If a value has not been suppressed, replace it with 0.
-    dplyr::mutate_at(supp_col, ~ as.numeric(stringr::str_detect(., regex_char)))
+  # Check User Input
+  checkmate::assert(
+    checkmate::check_data_frame(
+      df,
+      min.rows = 1,
+      min.cols = 1
+    ),
+    checkmate::check_character(
+      supp_col,
+      min.len = 1
+    ),
+    checkmate::check_subset(supp_col, names(df)),
+    combine = "and"
+  )
 
-  return (checker)
+  checkmate::assert(
+    checkmate::check_character(regex_char, min.chars = 1, all.missing = FALSE),
+    checkmate::check_numeric(regex_char),
+    combine = "or"
+  )
+
+  # If a value has been suppressed, replace it with 1.
+  # If a value has not been suppressed, replace it with 0.
+  df |>
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::all_of(supp_col),
+        \(col) {
+          as.numeric(
+            stringr::str_detect(col, regex_char)
+          )
+        }
+      )
+    )
 }
