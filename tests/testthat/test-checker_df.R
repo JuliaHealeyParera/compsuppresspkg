1e6b22ef55921bd63a6ab03826ef0b643a48c9b2
asenetcky <me@asenetcky.dev>
2025-07-03 06:58:15 -0400

    feat: implement test/helper.R

--- tests/testthat/test-checker_df.R
+++ tests/testthat/test-checker_df.R
@@ -1,36 +1,78 @@
 test_that("checker_df() returns dataframe", {
+  target_class <- "data.frame"
+  df <- suppressed_df()
+
   expect_equal(
-    class(checker_df(
-      data.frame(
-        x = c('*', '3', '4'),
-        y = c('4', '1', '*')
-      ),
-      c('x', 'y'),
-      '\\*'
-    )),
-    'data.frame'
+    class(df),
+    target_class
   )
 })
 
 test_that("checker_df() requires suppression columns", {
+  df <- suppressed_df()
+  rc <- "\\*"
+
+  # empty vector provided
   expect_error(
     checker_df(
-      data.frame(
-        x = c('*', '3', '4'),
-        y = c('4', '1', '*')
-      ),
-      c(),
-      '\\*'
+      df,
+      supp_col = c(),
+      regex_char = rc
+    )
+  )
+
+  # no argument provided
+  expect_error(
+    checker_df(
+      df,
+      regex_char = rc
     )
   )
 })
 
 test_that("checker_df() requires dataframe as input", {
+  # string instead of dataframe
   expect_error(
     checker_df(
       "string as improper input that should throw error",
       c('x', 'y'),
       '\\*'
+    )
+  )
+
+  # NULL instead of dataframe
+  expect_error(
+    checker_df(
+      NULL,
+      c('x', 'y'),
+      '\\*'
+    )
+  )
+
+  # dataframe but empty
+  expect_error(
+    checker_df(
+      data.frame(),
+      c('x'),
+      "\\&"
+    )
+  )
+
+  # dataframe should be good
+  expect_no_error(
+    checker_df(
+      df = unsuppressed_df(),
+      supp_col = c('x'),
+      regex_char = "\\^"
+    )
+  )
+
+  # tibble should be good
+  expect_no_error(
+    checker_df(
+      df = unsuppressed_df() |> dplyr::as_tibble(),
+      supp_col = c('x'),
+      regex_char = "\\^"
     )
   )
 })
