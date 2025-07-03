51c45c47f27a3aafd24f42591efc5071af5c192b
asenetcky <me@asenetcky.dev>
2025-07-02 21:54:47 -0400

    style: format with air

--- tests/testthat/test-checker_df.R
+++ tests/testthat/test-checker_df.R
@@ -3,27 +3,27 @@ test_that("checker_df() returns dataframe", {
     class(checker_df(
       data.frame(
         x = c('*', '3', '4'),
-        y = c('4','1','*')
-        ),
+        y = c('4', '1', '*')
+      ),
       c('x', 'y'),
       '\\*'
-      )),
-    'data.frame')
-  }
+    )),
+    'data.frame'
   )
+})
 
 test_that("checker_df() requires suppression columns", {
   expect_error(
     checker_df(
       data.frame(
         x = c('*', '3', '4'),
-        y = c('4','1','*')
+        y = c('4', '1', '*')
       ),
       c(),
       '\\*'
-    ))
-}
-)
+    )
+  )
+})
 
 test_that("checker_df() requires dataframe as input", {
   expect_error(
@@ -31,29 +31,29 @@ test_that("checker_df() requires dataframe as input", {
       "string as improper input that should throw error",
       c('x', 'y'),
       '\\*'
-    ))
-}
-)
+    )
+  )
+})
 
 test_that("checker_df() requires supp_col to be in df", {
   expect_error(
     checker_df(
       data.frame(
         x = c('*', '3', '4'),
-        y = c('4','1','*')
+        y = c('4', '1', '*')
       ),
       c('z', 'y'),
       '\\*'
-    ))
-}
-)
+    )
+  )
+})
 
 test_that("checker_df() allows number as regex character", {
   expect_equal(
     checker_df(
       data.frame(
         x = c('*', '3', '4'),
-        y = c('4','1','*')
+        y = c('4', '1', '*')
       ),
       c('x', 'y'),
       '4'
@@ -61,19 +61,19 @@ test_that("checker_df() allows number as regex character", {
     data.frame(
       x = c(0, 0, 1),
       y = c(1, 0, 0)
-    ))
-}
-)
+    )
+  )
+})
 
 test_that("checker_df() requires suppression character", {
   expect_error(
     checker_df(
       data.frame(
         x = c('*', '3', '4'),
-        y = c('4','1','*')
+        y = c('4', '1', '*')
       ),
       c('x', 'y'),
       ''
-    ))
-}
-)
+    )
+  )
+})
