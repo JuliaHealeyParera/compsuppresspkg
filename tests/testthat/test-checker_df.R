b76f6f41fe9d4442ede093698506946403d49f18
asenetcky <me@asenetcky.dev>
2025-07-03 07:08:48 -0400

    feat: implement rc()

--- tests/testthat/test-checker_df.R
+++ tests/testthat/test-checker_df.R
@@ -9,8 +9,9 @@ test_that("checker_df() returns dataframe", {
 })
 
 test_that("checker_df() requires suppression columns", {
+  # making it easy to swap in different parameters later
   df <- suppressed_df()
-  rc <- "\\*"
+  rc <- rc()
 
   # empty vector provided
   expect_error(
@@ -31,21 +32,24 @@ test_that("checker_df() requires suppression columns", {
 })
 
 test_that("checker_df() requires dataframe as input", {
+  # making it easy to swap in different parameters later
+  rc <- rc()
+
   # string instead of dataframe
   expect_error(
     checker_df(
-      "string as improper input that should throw error",
-      c('x', 'y'),
-      '\\*'
+      df = "string as improper input that should throw error",
+      supp_col = c('x', 'y'),
+      regex_char = rc
     )
   )
 
   # NULL instead of dataframe
   expect_error(
     checker_df(
-      NULL,
-      c('x', 'y'),
-      '\\*'
+      df = NULL,
+      supp_coll = c('x', 'y'),
+      regex_char = rc
     )
   )
 
@@ -53,8 +57,8 @@ test_that("checker_df() requires dataframe as input", {
   expect_error(
     checker_df(
       data.frame(),
-      c('x'),
-      "\\&"
+      supp_col = c('x'),
+      regex_char = rc
     )
   )
 
@@ -63,7 +67,7 @@ test_that("checker_df() requires dataframe as input", {
     checker_df(
       df = unsuppressed_df(),
       supp_col = c('x'),
-      regex_char = "\\^"
+      regex_char = rc
     )
   )
 
@@ -72,20 +76,30 @@ test_that("checker_df() requires dataframe as input", {
     checker_df(
       df = unsuppressed_df() |> dplyr::as_tibble(),
       supp_col = c('x'),
-      regex_char = "\\^"
+      regex_char = rc
     )
   )
 })
 
 test_that("checker_df() requires supp_col to be in df", {
+  df <- suppressed_df()
+  rc <- rc()
+  bad_cols <- c("y", "z")
+  good_cols <- c("x", "y")
+
   expect_error(
     checker_df(
-      data.frame(
-        x = c('*', '3', '4'),
-        y = c('4', '1', '*')
-      ),
-      c('z', 'y'),
-      '\\*'
+      df = df,
+      supp_col = bad_cols,
+      regex_char = rc
+    )
+  )
+
+  expect_no_error(
+    checker_df(
+      df = df,
+      supp_col = good_cols,
+      regex_char = rc
     )
   )
 })
